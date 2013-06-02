bc66f50720f709f810b10fc5b08338588aab769a
Steve Mckellar <steve@adaptivelab.co.uk>
2013-06-02 10:05:53 +0100

    Added buttons and lights

--- key/key.ino
+++ key/key.ino
@@ -1,18 +1,47 @@
 #include <JeeLib.h>
 
-MilliTimer sendTimer;
+#define BUTTON_PIN 1
+#define LED_PIN 2
+#define SPEAKER_PIN 3
+
 char password[] = "123DOOP";
+int button = false;
+
+void unlock () {
+    rf12_sendStart(0, password, sizeof password);
+    button = false;
+}
+
+void blink () {
+    digitalWrite(LED_PIN, 1);
+    delay(100);
+    digitalWrite(LED_PIN, 0);
+}
+
+void alert () {
+}
 
 void setup () {
     Serial.begin(57600);
     Serial.println("\nIAmAKeyBert");
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(SPEAKER_PIN, OUTPUT);
     rf12_initialize(1, RF12_868MHZ);
     rf12_encrypt(RF12_EEPROM_EKEY);
 }
 
 void loop () {
+    if(digitalRead(BUTTON_PIN) == 1) {
+        digitalWrite(LED_PIN, 1);
+        button = true;
+    }
+
     rf12_recvDone();
-    if (rf12_canSend() && sendTimer.poll(3000)) {
-        rf12_sendStart(0, password, sizeof password);
+
+    if(rf12_canSend() && button == true) {
+        unlock();
+        delay(100);
+        digitalWrite(LED_PIN, 0);
     }
 }
--- lock/lock.ino
+++ lock/lock.ino
@@ -1,32 +1,55 @@
 #include <JeeLib.h>
 
+#define RELAY_PIN 1
+#define LED_PIN 2
+
 byte recvCount;
 char password[16] = "123DOOP";
 char msg;
 bool allGood;
 
+void unlock (int time) {
+    digitalWrite(RELAY_PIN, 1);
+    delay(time);
+    digitalWrite(RELAY_PIN, 0);
+}
+
+void blink () {
+    digitalWrite(LED_PIN, 1);
+    delay(100);
+    digitalWrite(LED_PIN, 0);
+}
+
+bool checkPassword () {
+    allGood = true;
+    for (byte i = 0; i < rf12_len; ++i) {
+        msg = (int) rf12_data[i];
+        if(msg != password[i]) {
+            allGood = false;
+        }
+    }
+    return allGood;
+}
+
 void setup () {
     Serial.begin(57600);
     Serial.println("\nIAmALockBert");
+    pinMode(RELAY_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
     rf12_initialize(1, RF12_868MHZ);
     rf12_encrypt(RF12_EEPROM_EKEY);
 }
 
 void loop () {
     if (rf12_recvDone() && rf12_crc == 0) {
-        allGood = true;
-        for (byte i = 0; i < rf12_len; ++i) {
-            msg = (int) rf12_data[i];
-            if(msg != password[i]) {
-                allGood = false;
-            }
-        }
-        if(allGood) {
-            Serial.println("YEY!");
+        if(checkPassword()) {
             // Unlock
+            Serial.println("YEY!");
+            blink();
+            unlock(5000);
         } else {
-            Serial.println("NOT YEY!");
             // Alarms!
+            Serial.println("NOT YEY!");
         }
     }
 }
