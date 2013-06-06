3d77395ece44cc86bc5bcdda3949f2dac3776bc6
Steve Mckellar <steve@adaptivelab.co.uk>
2013-06-06 14:11:16 +0100

    reply to ip scan

--- key/key.ino
+++ key/key.ino
@@ -4,19 +4,26 @@
 #define BUTTON_PIN 5
 #define LED_PIN 6
 
+// supertweet.net username:password in base64
+#define KEY "aWFtbG9ja2JlcnQ6MmI1NCJjMSZjNA=="
+#define API_URL "/1.1/statuses/update.json"
+
 // mac address
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
-// ethernet interface ip address
-static byte myip[] = { 192,168,1,203 };
-// gateway ip address
-static byte gwip[] = { 192,168,1,1 };
+// // ethernet interface ip address
+// static byte myip[] = { 192,168,1,203 };
+// // gateway ip address
+// static byte gwip[] = { 192,168,1,1 };
 // password
 char password[] = "123DOOP";
+
+char website[] PROGMEM = "api.supertweet.net";
 
 int button = false;
 bool wasOpened = false;
 long unlocked = 0;
 long now = 0;
+Stash stash;
 byte Ethernet::buffer[500];
 BufferFiller bfill;
 
@@ -65,6 +72,39 @@ static word api() {
     return bfill.position();
 }
 
+static word ping() {
+    bfill = ether.tcpOffset();
+    bfill.emit_p(PSTR(
+        "$F"
+        "IAmKeybert"),
+        json_OK);
+    return bfill.position();
+}
+
+static void sendToTwitter () {
+    // generate two fake values as payload - by using a separate stash,
+    // we can determine the size of the generated message ahead of time
+    const byte *buf = ether.myip;
+
+    byte sd = stash.create();
+    stash.print("status=@domakesaythings i am lockbert my IP is: ");
+
+    for (byte i = 0; i < 4; ++i) {
+        Serial.print( buf[i], DEC );
+        if (i < 3)
+            stash.print('.');
+    }
+    stash.println();
+    stash.save();
+
+    // generate the header with payload - note that the stash size is used,
+    // and that a "stash descriptor" is passed in as argument using "$H"
+    Stash::prepare(PSTR(API_URL), website, PSTR(KEY), stash.size(), sd);
+
+    // send the packet - this also releases all stash buffers once done
+    ether.tcpSend();
+}
+
 void setup () {
     // Welcome
     Serial.begin(57600);
@@ -94,14 +134,18 @@ void setup () {
     //     Serial.println("Static setup failed");
     // }
 
-    // dhcp
-    // if (!ether.dhcpSetup()) {
-    //     Serial.println("DHCP failed");
-    // }
+    // DHCP
+    if (!ether.dhcpSetup()) {
+        Serial.println("DHCP failed");
+    }
 
     ether.printIp("IP:  ", ether.myip);
     ether.printIp("GW:  ", ether.gwip);  
     ether.printIp("DNS: ", ether.dnsip);
+
+    blink();
+
+    sendToTwitter();
 
     blink();
 }
@@ -125,20 +169,22 @@ void loop () {
         char *data = (char *) Ethernet::buffer + pos;
 
         // Serial.println(bfill);
-        // Serial.println(data);
+        Serial.println(data);
 
         Serial.println( "REQUEST" );
 
         if (strncmp("GET /", data, 5) == 0) {
             data += 5;
             if (strncmp("8ec49811 ", data, 9) == 0) {
-
                 Serial.println( "YEY!" );
                 unlock();
-
+                ether.httpServerReply(api());
+            } else if (strncmp("areyoukeybert ", data, 14) == 0) {
+                Serial.println( "IAmKeybert" );
+                ether.httpServerReply(ping());
+            } else {
+                ether.httpServerReply(api());
             }
-
-            ether.httpServerReply(api());
         }
     }
 
